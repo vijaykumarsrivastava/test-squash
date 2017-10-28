04f732d283638a80de8d33a67ca541f50e850041
Vijay Kumar <vijay.kumar2@synchronoss.com>
2017-10-28 12:17:23 +0530

    Added Binary Search

---
+++ src/main/java/com/vijay/test/squash/BinarySearch.java
@@ -0,0 +1,18 @@
+package com.vijay.test.squash;
+
+/**
+ * 
+ * @author vijay
+ *
+ */
+public class BinarySearch {
+
+	public int search(final int[] array, final int search, final int start, final int end) {
+		final int middle = start + (end - search) >> 1;
+		if (search == array[middle])
+			return middle;
+		else if (search > array[middle])
+			return search(array, search, middle + 1, end);
+		return search(array, search, start, middle - 1);
+	}
+}
